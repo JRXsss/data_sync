5a586e957608b5ef1ae82e26ca83b388053d0886
JRXsss <160558343+JRXsss@users.noreply.github.com>
2026-03-07 15:55:22 +0800

    Update sync_shopline_order_statistics.py

--- sync_shopline_order_statistics.py
+++ sync_shopline_order_statistics.py
@@ -7,7 +7,7 @@ from google.cloud import bigquery
 # Env
 # =========================
 MYSQL_HOST = os.getenv("MYSQL_HOST")
-MYSQL_PORT = int(os.getenv("MYSQL_PORT", "3306"))
+MYSQL_PORT = int(os.getenv("MYSQL_PORT") or "3306")
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_DB = os.getenv("MYSQL_DB", "caguuu_erp")
